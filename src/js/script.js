b4b143411411812e34fd17cfd07c2d253f8d9691
Jedbert <jedbert@yahoo.com>
2024-08-02 22:25:11 +0100

    Secondary commit 02/08/2024. 22:25pm

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+// scripts.js
+window.addEventListener("load", () => {
+  const preloader = document.getElementById("preloader");
+  const mainContent = document.getElementById("main");
+
+  preloader.style.display = "none"; // Hide preloader
+  mainContent.style.display = "block"; // Show main content
+});
